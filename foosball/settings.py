55c050391f303760b53800c5bccb61bd1f5b65a4
System Administrator <root@Sankets-MacBook-Air.local>
2019-04-18 19:25:58 +0530

    1

--- foosball/settings.py
+++ foosball/settings.py
@@ -124,6 +124,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join('manage_team/rest_framework/static')
+STATIC_ROOT = os.path.join('static')
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
